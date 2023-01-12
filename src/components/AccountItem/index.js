acaba963e3bf9773fcdf64245310a812a97b8124
KhaiVox <khaiduchoa@gmail.com>
2023-01-12 14:05:15 +0700

    Bài 98

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
+
+import classNames from 'classnames/bind'
+import styles from './AccountItem.module.scss'
+
+const cx = classNames.bind(styles)
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://img.freepik.com/free-vector/tiktok-banner-with-watercolor-splatter_69286-194.jpg?w=2000"
+                alt=""
+            />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    sfaf <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>hffh</span>
+            </div>
+        </div>
+    )
+}
+
+export default AccountItem
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import classNames from 'classnames/bind'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+import Tippy from '@tippyjs/react/headless'
 
 import styles from './Header.module.scss'
+import { Wrapper as PopperWrapper } from '~/components/Popper'
+import AccountItem from '~/components/AccountItem'
 const cx = classNames.bind(styles)
 
 function Header() {
+    const [searchResult, setSearchResult] = useState(1)
+
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
@@ -15,18 +21,33 @@ function Header() {
                     alt=""
                 />
 
-                <div className={cx('search')}>
-                    <input placeholder="Search accounts and videos" spellCheck={false} />
+                <Tippy
+                    visible={searchResult > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWrapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
 
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Search accounts and videos" spellCheck={false} />
 
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
-                </div>
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
 
                 <div className={cx('action')}></div>
             </div>
