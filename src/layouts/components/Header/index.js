d9deef5a8512056762e063279ad4b11b22b16798
KhaiVox <khaiduchoa@gmail.com>
2023-01-16 16:20:25 +0700

    Bài 110

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -14,7 +14,7 @@ import Tippy from '@tippyjs/react'
 import 'tippy.js/dist/tippy.css'
 import classNames from 'classnames/bind'
 import { Link } from 'react-router-dom'
-import routesConfig from '~/config/routes.js'
+import config from '~/config'
 
 import styles from './Header.module.scss'
 import Button from '~/components/Button'
@@ -97,7 +97,7 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <Link to={routesConfig.home} className={cx('logo-link')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
                     <img
                         className={cx('logo')}
                         src="https://sweetvioletbakes.com/wp-content/uploads/2020/04/tiktok-logo-9.png"
--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -5,7 +5,7 @@ import HeadlessTippy from '@tippyjs/react/headless'
 import 'tippy.js/dist/tippy.css'
 import { SearchIcon } from '~/components/Icons'
 
-import * as searchServices from '~/apiServices/searchServices'
+import * as searchServices from '~/services/searchService'
 import { Wrapper as PopperWrapper } from '~/components/Popper'
 import AccountItem from '~/components/AccountItem'
 import { useDebounce } from '~/hooks'
